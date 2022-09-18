05372a458f492179d770a2a91bf8799c64389da1
Juan Pablo Chavez Arteaga <juanp26842@gmail.com>
2022-09-17 21:50:55 -0400

    Captcha image created in user directory

--- captchamanagement.py
+++ captchamanagement.py
@@ -1,5 +1,10 @@
 from captcha.image import ImageCaptcha
+import os
 import random
+
+USER_PATH = os.path.expanduser('~')
+APP_FOLDER_PATH	= USER_PATH + '/.control docente/'
+CAPTCHA_PATH = APP_FOLDER_PATH + "captcha.png"
 
 class Captcha:
 
@@ -7,15 +12,17 @@ class Captcha:
 		self.captchaText = ""
 	
 	def generate(self):
-		image = ImageCaptcha(width = 250, height = 80)
+		image = ImageCaptcha(width = 250, height = 80, fonts=['C:/Windows/Fonts/Arial.ttf'])
 
 		for number in range(5):
 			digit = random.randint(0, 9)
 			self.captchaText += str(digit)
+		
+		if not os.path.isdir(APP_FOLDER_PATH):
+			os.makedirs(APP_FOLDER_PATH)
 
 		data = image.generate(self.captchaText)
-		
-		image.write(self.captchaText, "captcha.png")
+		image.write(self.captchaText, CAPTCHA_PATH)
 
 	def getText(self):
 		return self.captchaText
